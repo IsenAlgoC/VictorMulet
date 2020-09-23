595b7a09331f9d41fe23c531e2fafb1640815c29
Victor MULET <victor.mulet@isen.yncrea.fr>
2020-09-23 15:11:13 +0200

    Push

--- TP2/TP2/boucle.c
+++ TP2/TP2/boucle.c
@@ -1,3 +1,25 @@
-int main() {
+#include<stdio.h>
+#include<stdlib.h>
+int n;
+int sum1 = 0;
+int sum2 = 0;
+int sum3 = 0;
+n = 100;
+int i = 1;
+int j = 1;
 
+int main() {
+	for (i; i < n + 1; i++) {
+		sum1 += i;
+		}
+	printf("n = %d, sum = %d",n, sum1);
+	while (i < n) {
+		sum2 += i;
+		i++;
+	}
+	do {
+		sum3 += j;
+		j++;
+	}
+	while (!i == n);
 }
